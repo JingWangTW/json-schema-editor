d7846c423cb727dbc0cb82bb28ca6da3a0f0f04e
Jing Wang <tony.jing.wang@gmail.com>
2023-06-22 23:51:57 +0800

    refactor: 💡 Use minified bootstrap style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 /* eslint @typescript-eslint/explicit-function-return-type: 0 */
 
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 
